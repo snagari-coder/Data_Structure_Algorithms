f41dfee9195bf094a8322fd95a40cf2c9a2d51a3
snagari-coder <62784275+snagari-coder@users.noreply.github.com>
2021-06-06 18:34:27 -0700

    Create zig_zag_array.py

---
+++ Arrays/zig_zag_array.py
@@ -0,0 +1,34 @@
+# convert an array into zig-zag array
+# Given an array of DISTINCT elements, 
+# rearrange the elements of array in zig-zag fashion in O(n) time. 
+# The converted array should be in form a < b > c < d > e < f. 
+
+def zig_zag_array(arr):
+    n = len(arr)
+    i = 0
+    flag = 0
+    while i < n and i != n-1:
+        if flag == 0:
+            if arr[i] < arr[i+1]:
+                i += 1
+                flag = 1
+            else:
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                i += 1
+                flag = 1
+        if flag == 1 and i != n-1:
+            if arr[i] > arr[i+1]:
+                i += 1
+                flag = 0
+            else:
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                i += 1
+                flag = 0
+
+    return arr
+
+#print(zig_zag_array([3,4,6,2,1,8,9]))
+#print(zig_zag_array([4,3,7,8,6,2,1]))
+print(zig_zag_array([1,4,3,2]))
+#Time complexity = O(n)
+#Space complexity = O(1)
