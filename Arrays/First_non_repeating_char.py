d79441c2b8da7a2e0ab91f12ad7469c8524f4b4a
snagari-coder <62784275+snagari-coder@users.noreply.github.com>
2021-06-13 22:31:02 -0700

    Create First_non_repeating_char.py

---
+++ Arrays/First_non_repeating_char.py
@@ -0,0 +1,37 @@
+# Given an input stream of n characters consisting only of small case alphabets the task 
+# is to find the first non repeating character each time a character is inserted to the stream. 
+# If no non repeating element is found print -1.
+# Example
+# Flow in stream : a, a, b, c
+# a goes to stream : 1st non repeating element a (a)
+# a goes to stream : no non repeating element -1 (a, a)
+# b goes to stream : 1st non repeating element is b (a, a, b)
+# c goes to stream : 1st non repeating element is b (a, a, b, c)
+
+
+from collections import OrderedDict
+
+def first_non_repeating_char(input):
+    input_stream = input
+    hashmap = OrderedDict()
+    firstNonRecurringChar = input_stream[0]
+    for char in input_stream:
+        print("Reading "+ char + " from input stream")
+        if char not in hashmap:
+            hashmap[char] = 1
+        else:
+            hashmap[char] = hashmap[char] + 1
+        #print(hashmap)
+
+        if hashmap[firstNonRecurringChar] != 1:
+            for key,value in hashmap.items():
+                #print("key, value : ", key, value)
+                if value == 1:
+                    firstNonRecurringChar = key
+                    print("firstNonRecurringChar so far is: : ", firstNonRecurringChar)
+                    break
+        else:
+            print("firstNonRecurringChar so far is: ", firstNonRecurringChar)
+first_non_repeating_char("geekforgeekandgeeksandquizfor")
+# Time complexity = O(1) Looking up from hashmap
+# Space complexity = O(n)
