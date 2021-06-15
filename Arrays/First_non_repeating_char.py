6c88b762acec19861904a52d3e3192ac522778d6
snagari-coder <62784275+snagari-coder@users.noreply.github.com>
2021-06-15 16:26:48 -0700

    Update First_non_repeating_char.py

--- Arrays/First_non_repeating_char.py
+++ Arrays/First_non_repeating_char.py
@@ -1,13 +1,11 @@
-# Given an input stream of n characters consisting only of small case alphabets the task 
-# is to find the first non repeating character each time a character is inserted to the stream. 
-# If no non repeating element is found print -1.
-# Example
+# Given an input stream of n characters consisting only of small case alphabets the task is to find the first
+# non repeating character each time a character is inserted to the stream. If no non repeating element is found print -1.
+#Example
 # Flow in stream : a, a, b, c
 # a goes to stream : 1st non repeating element a (a)
 # a goes to stream : no non repeating element -1 (a, a)
 # b goes to stream : 1st non repeating element is b (a, a, b)
 # c goes to stream : 1st non repeating element is b (a, a, b, c)
-
 
 from collections import OrderedDict
 
@@ -30,8 +28,12 @@ def first_non_repeating_char(input):
                     firstNonRecurringChar = key
                     print("firstNonRecurringChar so far is: : ", firstNonRecurringChar)
                     break
+                elif value != 1:
+                    print("There is no non-repeating char in the given sequence")
+                    return -1
         else:
             print("firstNonRecurringChar so far is: ", firstNonRecurringChar)
-first_non_repeating_char("geekforgeekandgeeksandquizfor")
+#first_non_repeating_char("geekforgeekandgeeksandquizfor")
+first_non_repeating_char("ababab")
 # Time complexity = O(1) Looking up from hashmap
 # Space complexity = O(n)
