db5c4bfc421d1dd6d473c754bf6c8a732b56f998
snagari-coder <62784275+snagari-coder@users.noreply.github.com>
2021-06-15 16:20:27 -0700

    Create common_strings_in_list_of_strings.py

---
+++ Arrays/common_strings_in_list_of_strings.py
@@ -0,0 +1,28 @@
+# Given two list of strings, find a list of common strings between the input lists.
+# Ex: list1 = ["dog", "cat", "tiget", "elephant"],list2 = ["horse", "dog", "cow", "cat"], result = ['dog', 'cat']
+def find_common_animals(list1, list2):
+    hashmap = {}
+    common_animals = []
+    for animal in list1:
+        hashmap[animal] = 1
+
+    for animal in list2:
+        if animal in hashmap:
+            hashmap[animal] = hashmap[animal] + 1
+
+    for key,value in hashmap.items():
+        if value == 2:
+            common_animals.append(key)
+
+    return common_animals
+#Time complexity = O(n)
+#Space complexity = O(n)
+print(find_common_animals(["dog", "cat", "tiget", "elephant"],["horse", "dog", "cow", "cat"]))
+
+
+def find_common_animals_with_sets(list1, list2):
+    common_animals = set(list1) & set(list2)
+    print(list(common_animals))
+#Time complexity = O(min(len(list1),len(list2)))
+#Space complexity = less than or equal to min(len(list1),len(list2))
+find_common_animals_with_sets(["dog", "cat", "tiget", "elephant"],["horse", "dog", "cow", "cat"])
