a0c5c96d5c3dfae894f9579f11aff940fc8c230b
snagari-coder <62784275+snagari-coder@users.noreply.github.com>
2021-06-15 22:13:18 -0700

    Create Is_Anagram.py

---
+++ Arrays/Is_Anagram.py
@@ -0,0 +1,34 @@
+# Check if two strings are anagram or not.
+# Anagram is if two strings have same letters, but are rearranged.
+# Ex: C A T and A C T are Anagram.
+def is_anagram(string1,string2):
+    list1 = list(string1)
+    list2 = list(string2)
+
+    hashmap = {}
+
+    if len(list1) != len(list2):
+        return False
+    else:
+        for i in range(len(list1)):
+            if list1[i] in hashmap:
+                hashmap[list1[i]] = hashmap[list1[i]] + 1
+            else:
+                hashmap[list1[i]] =  1
+        print(hashmap)
+
+        for i in range(len(list2)):
+            if list2[i] not in hashmap:
+                return False
+            else:
+                hashmap[list2[i]] = hashmap[list2[i]] - 1
+        print(hashmap)
+        for key,value in hashmap.items():
+            if value != 0:
+                return False
+            else:
+                return True
+#print(is_anagram("silent", "listen"))
+print(is_anagram("letter", "teller"))
+# Time complexity = O(n)
+# Space complexity = O(n)
