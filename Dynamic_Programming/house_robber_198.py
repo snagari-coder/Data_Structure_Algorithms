31e8739ab6956e2d99aab80fcd946a7bb203157c
snagari-coder <62784275+snagari-coder@users.noreply.github.com>
2021-08-13 17:44:48 -0700

    Update house_robber_198.py

--- Dynamic_Programming/house_robber_198.py
+++ Dynamic_Programming/house_robber_198.py
@@ -102,6 +102,7 @@ def rob(self, nums: List[int]) -> int:
 
  ################################################################################################################################################################################
 '''
+Leetcode 337
 The thief has found himself a new place for his thievery again. There is only one entrance to this area, called root.
 Besides the root, each house has one and only one parent house. After a tour, the smart thief realized that all houses 
 in this place form a binary tree. It will automatically contact the police if two directly-linked houses were broken into on the same night.
