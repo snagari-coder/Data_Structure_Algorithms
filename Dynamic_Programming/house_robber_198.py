6a0797040dddbc7048be9484faf467e93c32c096
snagari-coder <62784275+snagari-coder@users.noreply.github.com>
2021-08-13 17:43:53 -0700

    Update house_robber_198.py

--- Dynamic_Programming/house_robber_198.py
+++ Dynamic_Programming/house_robber_198.py
@@ -99,3 +99,38 @@ def rob(self, nums: List[int]) -> int:
         actual_max_loot = max(solve(nums[0:n-1]), solve(nums[1:n])) # Given 12345, you can rob from 1-4 or 2-5 because 1,5 are adjacent. 
         
         return actual_max_loot
+
+ ################################################################################################################################################################################
+'''
+The thief has found himself a new place for his thievery again. There is only one entrance to this area, called root.
+Besides the root, each house has one and only one parent house. After a tour, the smart thief realized that all houses 
+in this place form a binary tree. It will automatically contact the police if two directly-linked houses were broken into on the same night.
+Given the root of the binary tree, return the maximum amount of money the thief can rob without alerting the police.
+
+'''
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def rob(self, root: Optional[TreeNode]) -> int:
+        def dfs(root):
+            if not root:
+                return [0, 0]
+
+            leftPair = dfs(root.left)
+            rightPair = dfs(root.right)
+
+            withRoot = root.val + leftPair[1] + rightPair[1]
+            withoutRoot = max(leftPair) + max(rightPair)
+
+            return [withRoot, withoutRoot]
+
+        return max(dfs(root))
+    
+ # Time complexity = O(n)
+ 
