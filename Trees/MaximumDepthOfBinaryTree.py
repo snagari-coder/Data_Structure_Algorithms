5ba808ca75d4f3490dd0a55e9aa1c877254625b9
snagari-coder <62784275+snagari-coder@users.noreply.github.com>
2021-08-15 20:15:54 -0700

    Update MaximumDepthOfBinaryTree.py

--- Trees/MaximumDepthOfBinaryTree.py
+++ Trees/MaximumDepthOfBinaryTree.py
@@ -23,3 +23,28 @@ class Solution:
         return height
 # Time complexity = O(n), n is number of nodes
 # Space complexity = O(1), without considering the stack space for recursive action
+
+###########################################################################################################################################################################
+############# MINIMUM DEPTH OF A TREE ########################
+
+
+# Definition for a binary tree node.
+ class TreeNode:
+     def __init__(self, val=0, left=None, right=None):
+         self.val = val
+         self.left = left
+         self.right = right
+class Solution:
+    def minDepth(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0
+        if root and (root.left is None and root.right is None):
+            return 1
+        left = self.minDepth(root.left)
+        right = self.minDepth(root.right)
+        if left == 0:
+            return right + 1
+        elif right == 0:
+            return left + 1
+        else:
+            return min(left,right) + 1
