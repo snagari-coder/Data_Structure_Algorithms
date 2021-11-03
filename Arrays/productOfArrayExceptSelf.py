17f9b6d02607a523ed89c8b02a585f11d606f32a
snagari-coder <62784275+snagari-coder@users.noreply.github.com>
2021-11-02 23:27:11 -0700

    Update productOfArrayExceptSelf.py

--- Arrays/productOfArrayExceptSelf.py
+++ Arrays/productOfArrayExceptSelf.py
@@ -12,40 +12,39 @@ Output: [0,0,9,0,0]
 '''
 
 def productExceptSelf_better(nums):
-    previous, product = 1, 1
-    output = []
-    n = len(nums)
-    for i in range(n):
-        previous *= nums[i]
-        output.append(previous)
-
-    for i in range(n - 1, -1, -1):
-        if i == n - 1:
-            print(output[i - 1])
-            output[i] = output[i - 1]
-            product *= nums[i]
-        elif i == 0:
-            output[i] = product
-        else:
-            insert_element = output[i - 1] * product
-            output[i] = insert_element
-            product *= nums[i]
-
-    return output
-
-
-def productExceptSelf(nums):
-    ## If division is allowed ##
-    product = 1
-    result = []
-    for i in range(len(nums)):
-        product *= nums[i]
-
-    for i in range(len(nums)):
-        if nums[i] != 0:
-            result.append(product // nums[i])
+    result = [1]*len(nums)
+    for i in range(1,len(nums)): #assuming there is 1 preceeding the nums array
+        result[i] = result[i-1]*nums[i-1] #creating prefix array
+    temp = 1 #assuming there is 1 succeding the nums array
+    for i in range(len(nums)-1,-1,-1):
+        result[i] = temp * result[i]
+        temp = temp * nums[i]
 
     return result
+#Time = 2 * O(n), Space = O(1)
+
+###################################################################################
+def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        prefix = [1]*len(nums)
+        suffix = [1] * len(nums)
+        result = [1] * len(nums)
+        for i in range(1,len(nums)):
+            prefix[i] = prefix[i-1]*nums[i-1]
+
+        for i in range(len(nums)-2,-1,-1):
+            suffix[i] = suffix[i+1]*nums[i+1]
+
+        for i in range(len(nums)):
+            result[i] = prefix[i] * suffix[i]
+        
+
+        return result
+
+    # Time = space = 3 * O(n) = O(n)
 
 
 print(productExceptSelf([1, 2, 3, 4]))
