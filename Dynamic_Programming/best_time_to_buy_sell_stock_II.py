1befcf090f446021f0322c19234f57cb5468135e
snagari-coder <62784275+snagari-coder@users.noreply.github.com>
2021-11-29 22:11:56 -0800

    Update best_time_to_buy_sell_stock_II.py

--- Dynamic_Programming/best_time_to_buy_sell_stock_II.py
+++ Dynamic_Programming/best_time_to_buy_sell_stock_II.py
@@ -1,3 +1,28 @@
+#Leetcode 121:
+'''
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+'''
+class Solution(object):
+    import sys
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        minPrice,maxProfit = float('inf'),0
+        for i in range(len(prices)):
+            if prices[i] < minPrice:
+                minPrice = prices[i]
+            elif prices[i] - minPrice > maxProfit:
+                maxProfit = prices[i] - minPrice
+                
+        return maxProfit
+    #time = O(n)
+###################################################
 # Leetcode : 122
 #You are given an array prices where prices[i] is the price of a given stock on the ith day.
 #Find the maximum profit you can achieve. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).
