c3ab093a2aa129a190f864d4d365da1fb7fc689d
snagari-coder <62784275+snagari-coder@users.noreply.github.com>
2021-11-29 19:38:30 -0800

    Create 2sum.py

---
+++ Sorting/2sum.py
@@ -0,0 +1,65 @@
+
+'''
+Leetcode 1. Two Sum
+'''
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        
+        hashmap={}
+        for i in range(len(nums)):
+            if target-nums[i] in hashmap:
+                result = (i,hashmap[target-nums[i]])
+            else:
+                hashmap[nums[i]] = i
+        return result
+  # Time = space = O(n)
+
+##############################################################
+'''
+Leetcode 167. Two Sum II - Input Array Is Sorted
+'''
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        i,j=0,len(numbers)-1
+        while i < j:
+            if numbers[i]+numbers[j] == target:
+                return (i+1,j+1)
+            elif numbers[i]+numbers[j] > target:
+                j -= 1
+            else: 
+                i += 1
+                
+ # Time complexity = O(n), space = O(1)
+
+#########################################################################
+'''
+Leetcode 1099. Two Sum Less Than K
+'''
+class Solution(object):
+    def twoSumLessThanK(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        nums.sort()
+        i,j = 0, len(nums)-1
+        answer = -1
+        while i < j:
+            sum = nums[i] + nums[j]
+            if sum < k:
+                answer = max(answer,sum)
+                i += 1
+            else:
+                j-= 1
+        return answer
