38231eb0603704479c02af7623594d28d408ac44
snagari-coder <62784275+snagari-coder@users.noreply.github.com>
2022-01-20 05:35:39 -0800

    Update Is_Anagram.py

--- Arrays/Is_Anagram.py
+++ Arrays/Is_Anagram.py
@@ -1,6 +1,33 @@
 # Check if two strings are anagram or not.
 # Anagram is if two strings have same letters, but are rearranged.
 # Ex: C A T and A C T are Anagram.
+
+def IsAnagram(s1, s2):
+    if len(s1) != len(s2):
+        return False
+    hashmap = {}
+    for ele in s1:
+        if ele not in hashmap:
+            hashmap[ele] = 1
+        else:
+            hashmap[ele] += 1
+    print(hashmap)
+
+    for ele in s2:
+        if ele not in hashmap:
+            return False
+        else:
+            hashmap.popitem()
+
+    if len(hashmap) != 0:
+        return False
+    else:
+        return True
+
+print(IsAnagram('CAD', 'DAC'))
+
+
+======================================================================================================================================
 def is_anagram(string1,string2):
     list1 = list(string1) # You don't really have to convert to list. You can do the same thing with string.
     list2 = list(string2)
