4a57c1b071f61f6b73d8fb9d8e1f3dfcc6ab30a6
snagari-coder <62784275+snagari-coder@users.noreply.github.com>
2022-02-21 14:07:08 -0800

    Update roman_to_integers.py

--- strings/roman_to_integers.py
+++ strings/roman_to_integers.py
@@ -1,4 +1,36 @@
 
+# Leetcode 13
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        values = { "I": 1,
+                    "V": 5,
+                    "X": 10,
+                    "L": 50,
+                    "C": 100,
+                    "D": 500,
+                    "M": 1000,
+                    "IV": 4,
+                    "IX": 9,
+                    "XL": 40, 
+                    "XC": 90,
+                    "CD": 400,
+                    "CM": 900
+                 }
+        i = 0
+        total = 0
+        while i < len(s):
+            if i < len(s)-1 and s[i:i+2] in values:
+                total += values[s[i:i+2]]
+                i = i+2
+            else:
+                total += values[s[i]]
+                i = i + 1
+        return total
+    
+        # Time = O(1) = lookup from hashmap; finite string length
+        # space = O(1)
+
+##################################################
 import re
 def roman_to_integers(roman):
 
