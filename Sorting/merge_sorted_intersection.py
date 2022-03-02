d42fe288bb6b1442586236011ee643a7e433ee3a
snagari-coder <62784275+snagari-coder@users.noreply.github.com>
2022-03-01 20:43:34 -0800

    Update merge_sorted_intersection.py

--- Sorting/merge_sorted_intersection.py
+++ Sorting/merge_sorted_intersection.py
@@ -1,3 +1,6 @@
+# 4 questions here
+
+
 def merge_sorted_no_duplicates(l1, l2):
     if not l1:
         return l2
@@ -63,5 +66,28 @@ def intersect(l1, l2):
     return aux
 print(intersect([2, 3, 5, 6, 7, 8, 12], [5, 6, 8, 9, 10])) # [5, 6, 8]
 
-
-
+##############################################################################################
+# Leetcode 88
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+        p1 = m-1
+        p2 = n-1
+        for p in range(m+n-1,-1,-1):
+            if p2 < 0:
+                break
+            if p1 >= 0 and nums2[p2] < nums1[p1]:
+                nums1[p] = nums1[p1]
+                p1 -= 1
+            else:
+                nums1[p] = nums2[p2]
+                p2 -= 1
+                
+# Time = O(m+n)
+# Space = O(1)
