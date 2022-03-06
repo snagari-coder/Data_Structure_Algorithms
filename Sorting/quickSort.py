14edf307ee15ef565e4c35972fce1352a335c141
snagari-coder <62784275+snagari-coder@users.noreply.github.com>
2022-03-06 14:29:12 -0800

    Update quickSort.py

--- Sorting/quickSort.py
+++ Sorting/quickSort.py
@@ -1,3 +1,27 @@
+#Leetcode 912
+# Lomuto's partitioning
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        
+        self.quick_sort(nums,0,len(nums)-1)
+        return nums
+        
+    def quick_sort(self,nums,start,end):
+        if start >= end:
+            return
+        pivot = random.randint(start,end)
+        nums[pivot],nums[start] = nums[start],nums[pivot]
+        i = start
+        for j in range(start+1,end+1):
+            if nums[j] < nums[start]:
+                i += 1
+                nums[i],nums[j]=nums[j],nums[i]
+        nums[i],nums[start]=nums[start],nums[i]
+        
+        self.quick_sort(nums,start,i-1)
+        self.quick_sort(nums,i+1,end)
+#######################################################################################################################
+
 import random
 def quickSort(nums):
     def helper(A,start,end):
