f3cc0a349bab0bfab2da9feec157bdb5d6b577f2
snagari-coder <62784275+snagari-coder@users.noreply.github.com>
2022-03-12 06:55:51 -0800

    Update DFS_path_sum.py

--- Trees/DFS_path_sum.py
+++ Trees/DFS_path_sum.py
@@ -27,3 +27,35 @@ class Solution:
         return Flag[0]
       
       # Time complexity = O(n) = space complexity
+
+    ################## ALTERNATE ##############################
+    
+    # Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        if root is None:
+            return None
+        result = [False]
+        self.top_down_dfs(root,targetSum,result)
+        return result[0]
+    
+    def top_down_dfs(self,node,targetSum,result):
+        targetSum -= node.val
+        #Base case if leaf node
+        if node.left is None and node.right is None:
+            if targetSum == 0:
+                result[0] = True
+        
+        #intermediate nodes
+        if node.left is not None:
+            self.top_down_dfs(node.left,targetSum,result)
+        if node.right is not None:
+            self.top_down_dfs(node.right,targetSum,result)
+            
+            
+        
